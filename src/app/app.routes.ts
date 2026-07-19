28e244cc5eeaadef40791afcdc278068fe6ae27e
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-19 13:20:27 +0530

    refactor: simplify root app template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Employees } from './components/employees/employees';
+import { EmployeeForm } from './components/employee-form/employee-form';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'employees',
+    component: Employees,
+  },
+  {
+    path: 'employees/add',
+    component: EmployeeForm,
+  },
+  {
+    path: 'employees/edit/:id',
+    component: EmployeeForm,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
