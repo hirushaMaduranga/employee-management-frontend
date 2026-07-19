be7c7ab3c3ad48d402527d3f26b769d938f11b51
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-19 13:22:54 +0530

    feat: implement login functionality

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,60 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  username: string = '';
+  password: string = '';
+
+  loading: boolean = false;
+  errorMessage: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  login(): void {
+    this.errorMessage = '';
+
+    // Check empty fields
+    if (!this.username.trim() || !this.password.trim()) {
+      this.errorMessage = 'Username and password are required.';
+      return;
+    }
+
+    this.loading = true;
+
+    // Call Spring Boot login API
+    this.authService
+      .login({
+        username: this.username,
+        password: this.password,
+      })
+      .subscribe({
+        next: (response) => {
+          this.loading = false;
+
+          // Save logged-in username
+          localStorage.setItem('username', response.username);
+
+          // Redirect to dashboard
+          this.router.navigate(['/dashboard']);
+        },
+
+        error: () => {
+          this.loading = false;
+          this.errorMessage = 'Invalid username or password.';
+        },
+      });
+  }
+}
