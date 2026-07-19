06335076330519abf1a7e1cc6057a8a5860421ca
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-20 03:00:01 +0530

    feat: add employee view route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Employees } from './components/employees/employees';
 import { EmployeeForm } from './components/employee-form/employee-form';
+import { EmployeeView } from './components/employee-view/employee-view';
 
 export const routes: Routes = [
   {
@@ -11,26 +12,37 @@ export const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
+
   {
     path: 'login',
     component: Login,
   },
+
   {
     path: 'dashboard',
     component: Dashboard,
   },
+
   {
     path: 'employees',
     component: Employees,
   },
+
   {
     path: 'employees/add',
     component: EmployeeForm,
   },
+
+  {
+    path: 'employees/view/:id',
+    component: EmployeeView,
+  },
+
   {
     path: 'employees/edit/:id',
     component: EmployeeForm,
   },
+
   {
     path: '**',
     redirectTo: 'login',
