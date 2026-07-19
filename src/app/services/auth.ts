6019e2f6e7dfad14fe83537bb8fe19685d360716
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-19 13:20:45 +0530

    feat: add authentication service

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  message: string;
+  username: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class AuthService {
+  private readonly apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, data);
+  }
+}
